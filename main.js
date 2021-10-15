84ef2e9a26e1b44371907e96321c0713ba3ebd6b
Yeetomeeto <92560744+Yeetomeeto@users.noreply.github.com>
2021-10-15 10:48:04 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function add(){
+    var result =
+    Number(document.getElementById("n1").value) +
+    Number(document.getElementById("n2").value);
+    document.getElementById("answer").innerHTML=result;
+    
+}
